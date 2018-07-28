05edf07eb0e3d5eb64b1c6fca16902e29fc5f425
pdm Amlo <pdm.amlo@gmail.com>
2018-07-27 17:36:59 -0700

    Adding basic popup functionality
    Stil requires a lot of works but works,
    this milestone creates a basic csv file with all products stored in the
    db

--- js/Persistence.js
+++ js/Persistence.js
@@ -3,7 +3,7 @@ class Persistence
 {
 	constructor()
 	{
-		this.db = new DatabaseStore
+		this.database= new DatabaseStore
 		({
 			name		: "products"
 			,version	: 1
@@ -23,17 +23,17 @@ class Persistence
 			}
 		});
 
-		this.db.init();
+		this.database.init();
 	}
 
 	updateProduct(product)
 	{
-		return this.db.get("products",product.asin ).then((oldProduct)=>
+		return this.database.get("products",product.asin ).then((oldProduct)=>
 		{
 			if( oldProduct )
 				this.mergeProducts(oldProduct, product );
 
-			return this.db.put("products", product );
+			return this.database.put("products", product );
 		}).catch((e)=>
 		{
 			console.error("product not found", e );
@@ -154,5 +154,153 @@ class Persistence
 				}
 			}
 		);
+	}
+
+	getProductList( options )
+	{
+		return this.database.getAll("products",{});
+	}
+
+	getProductListBySearch( search )
+	{
+		return this.database.getAll("products", {index:"search", "=": search });
+	}
+
+	getProductListByParsedDate( search,options)
+	{
+		let opt	= {
+			index	: "parsed"
+		}
+		if( options.start )
+		{
+			opt[">="] = options.start;
+		}
+
+		if( options.end )
+		{
+			opt["<"] = options.end;
+		}
+
+		return this.database.getAll("products", options );
+	}
+
+
+
+	generateRawReport(products)
+	{
+		let csv	= [];
+		let keys 		= Object.keys( this.getValidKeys() );
+
+		let s = keys.join('\t')+'\n';
+
+		products.forEach((p)=>
+		{
+			let row = [];
+			keys.forEach((key)=>
+			{
+				if( key in p )
+				{
+					let value =  typeof p[key] === "string" ? p[key].trim().replace(/"/g, '""' ).replace(/\t/,' ') : p[ key ];
+					row.push( '"'+value+'"' );
+				}
+				else
+				{
+					row.push( '""' );
+				}
+			});
+			s+= row.join("\t")+"\n";
+		});
+
+		return s;
+	}
+
+	getValidKeys()
+	{
+		return {
+			asin: true
+			,url:true
+			,producer: true
+			,title	: true
+			,stock	: true
+			,no_offers	: true
+			,no_offers_text: true
+			,rating	: true
+			,number_of_ratings: true
+		}
+		/*
+		return {
+			'asin':1
+			,'url_keyword':1
+			,'producer':1
+			//,'producer_url':1
+			,'sale':1
+			,'shipping':1
+			,'price':1
+			,'url':1
+			,'title':1
+			,'rating':1
+			,'left qty':1
+			,'left':1
+			,'choice':1
+			,'number_of_ratings':1
+			,'fullfilled_by':1
+			,'no_offers_text':1
+			,'no_offers':1
+			,'no_offers_url':1
+			,'Rank':1
+			,'Rank Department':1
+			,'vendors':1
+			,'description':1
+			,'Sold by':1
+			,'fetures_1':1
+			,'fetures_2':1
+			,'fetures_3':1
+			,'fetures_4':1
+			,'fetures_5':1
+			,'image_0':1
+			,'image_1':1
+			,'image_2':1
+			,'image_3':1
+			,'image_4':1
+			,'image_5':1
+			,'image_6':1
+			,'versions':1
+			,'Part Number':1
+			,'Number of Items':1
+			,'Brand Name':1
+			,'EAN':1
+			,'Material':1
+			,'Model Number':1
+			,'Style':1
+			,'UNSPSC Code':1
+			,'UPC':1
+			,'Shipping Weight:':1
+			,'ASIN:':1
+			,'Item model number':1
+			,'Average Customer Review':1
+			,'Best Sellers Rank':1
+			,'Measurement System':1
+			,'Item Weight':1
+			//,'Thread Type':1
+			,'Product Dimensions':1
+			,'Color':1
+			,'Size':1
+			,'Domestic Shipping':1
+			,'International Shipping':1
+			,'Shipping Information':1
+			,'Item Weight':1
+			,'Height':1
+			//,'Item Shape':1
+			//,'Maximum Temperature':1
+			,'Width':1
+			//,'Shipping Advisory':1
+			//,'Specification Met':1
+			,'Length':1
+			//,'Lower Temperature Range':1
+			//,'Upper Temperature Rating':1
+			//,'Included Components':1
+			//,'Lower Temperature Rating':1
+			,'Parsed Date';:1
+			*/
 	}
 }
--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,32 @@
 
 document.addEventListener('DOMContentLoaded', function()
 {
-
-	var ext = new Client();
+	//var ext = new Client();
+	let persistence = new Persistence();
 
 	chrome.windows.getCurrent((w)=>
 	{
-		ext.executeOnBackground('RegisterWindow', { window_id : w.id	});
+		//ext.executeOnBackground('RegisterWindow', { window_id : w.id	});
 	});
+
+	document.getElementById('downloadButton').addEventListener('click',(evt)=>
+	{
+		persistence.getProductList().then((products)=>
+		{
+			let s = persistence.generateRawReport( products );
+			download('something.csv',s );
+		});
+	});
+
+	function download(filename, text)
+	{
+		var element = document.createElement('a');
+		element.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(text));
+		element.setAttribute('download', filename);
+		element.style.display = 'none';
+
+		document.body.appendChild(element);
+		element.click();
+		document.body.removeChild(element);
+	}
 });
