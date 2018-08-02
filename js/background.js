f0942507b8aa17bc8ad697173ec5845f68051dcb
pdm Amlo <pdm.amlo@gmail.com>
2018-08-02 14:47:22 -0700

    Save urls to detect pageType errors

--- js/background.js
+++ js/background.js
@@ -3,19 +3,16 @@ var persistence	= new Persistence();
 var ext			= new Server();
 
 
-ext.addListener("AddProduct",(url,request,tab_id)=>
+ext.addListener('UrlDetected',(url,request,tab_id)=>
 {
+	persistence.updateUrl( request ).then(()=>
+	{
 
+	});
 });
 
-
-ext.addListener("addProductList",(url,request,tab_id)=>
+ext.addListener('ProductsFound',(url,request,tab_id)=>
 {
-
-});
-
-ext.addListener("ProductsFound",(url,request,tab_id)=>
-{
-	console.log("products found", request );
+	console.log('products found', request );
 	persistence.updateProductLists( request );
 });
