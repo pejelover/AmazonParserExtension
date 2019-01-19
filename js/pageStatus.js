0e1c5cdea0dbaa77655697e4899f778873031691
pdm Amlo <pdm.amlo@gmail.com>
2019-01-18 16:58:34 -0800

    Adding buggie pageStatus

--- js/pageStatus.js
+++ js/pageStatus.js
@@ -7,27 +7,37 @@ document.addEventListener('DOMContentLoaded', function()
 
 	persistence.init().then(()=>
 	{
-		persistence.database.getDatabaseResume().then((dbResume)=>
+		//persistence.database.getDatabaseResume().then((dbResume)=>
+		//{
+		//	console.log( dbResume );
+		//	let s = '<ul>';
+
+		//	dbResume.forEach((store)=>
+		//	{
+		//		s += `<li>${store.name} ${store.total} </li>`;
+
+		//		if( store.indexes.length )
+		//		{
+		//			s+='<ul>';
+		//			store.indexes.forEach((index)=>
+		//			{
+		//				s += `<li>${index.name} ${ index.count } </li>`;
+		//			});
+		//			s+='</ul>';
+		//		}
+		//	});
+		//	s+='</ul>';
+		//	Utils.getById('pageStatusContainer').innerHTML = s;
+		//});
+
+		persistence.database.createBackup().then(( obj )=>
 		{
-			console.log( dbResume );
-			let s = '<ul>';
-
-			dbResume.forEach((store)=>
-			{
-				s += `<li>${store.name} ${store.total} </li>`;
-
-				if( store.indexes.length )
-				{
-					s+='<ul>';
-					store.indexes.forEach((index)=>
-					{
-						s += `<li>${index.name} ${ index.count } </li>`;
-					});
-					s+='</ul>';
-				}
-			});
-			s+='</ul>';
-			Utils.getById('pageStatusContainer').innerHTML = s;
+			let string = JSON.stringify( obj );
+			console.log( string.length );
+			let blob = new Blob([string],{ type:'application/json'});
+			string = null;
+			let objectURL = URL.createObjectURL( blob );
+			Utils.getById('pageStatusContainer').innerHTML = '<a href="'+objectURL+'" download="file.json">Download</a>';
 		});
 	})
 	.catch((error)=>
